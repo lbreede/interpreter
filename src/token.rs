6b134a5e589e3ff021d33a43872340c726b9ea5d
lbreede <67719520+lbreede@users.noreply.github.com>
2024-12-03 09:14:28 -0800

    impl FromStr for Token because why not

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(PartialEq, Debug, Clone)]
 pub enum Token {
     Illegal(String),
@@ -78,44 +80,23 @@ impl From<u8> for Token {
     }
 }
 
-// #[derive(Debug)]
-// pub struct ParseTokenError;
-// impl FromStr for Token {
-//     type Err = ParseTokenError;
-//
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         let tok = match s {
-//             "let" => Token::Let,
-//             "fn" => Token::Function,
-//             "true" => Token::True,
-//             "false" => Token::False,
-//             "if" => Token::If,
-//             "else" => Token::Else,
-//             "return" => Token::Return,
-//             _ => Token::Ident(s.to_string()),
-//         };
-//         Ok(tok)
-//     }
-// }
-//
-// #[allow(dead_code)]
-// impl Token {
-//     fn from_char(c: char) -> Result<Self, ParseTokenError> {
-//         let tok = match c {
-//             ';' => Token::Semicolon,
-//             '(' => Token::LParen,
-//             ')' => Token::RParen,
-//             ',' => Token::Comma,
-//             '{' => Token::LBrace,
-//             '}' => Token::RBrace,
-//             '+' => Token::Plus,
-//             '-' => Token::Minus,
-//             '*' => Token::Asterisk,
-//             '<' => Token::LessThan,
-//             '>' => Token::GreaterThan,
-//             '/' => Token::Slash,
-//             _ => return Err(ParseTokenError),
-//         };
-//         Ok(tok)
-//     }
-// }
+#[derive(Debug)]
+pub struct ParseTokenError;
+
+impl FromStr for Token {
+    type Err = ParseTokenError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tok = match s {
+            "let" => Token::Let,
+            "fn" => Token::Function,
+            "true" => Token::True,
+            "false" => Token::False,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
+            _ => Token::Ident(s.to_string()),
+        };
+        Ok(tok)
+    }
+}
